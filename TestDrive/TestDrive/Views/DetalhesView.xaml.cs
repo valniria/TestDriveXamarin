dc5adb6bdd194dc1a178a762a13f979db1b8f6ad
Valniria Bandeira <valniria.bandeira@multicomnet.com.br>
2020-11-04 22:55:18 -0300

    Andamento do App

---
+++ TestDrive/TestDrive/Views/DetalhesView.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestDrive.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalhesView : ContentPage
+    {
+        private const int FreioAbs = 800;
+        private const int ArCondicionado = 1000;
+        private const int Mp3Player = 500;
+
+        public Veiculo Veiculo { get; set; }
+
+        public string TextoFreioAbs { 
+            get {
+                return string.Format("Freio ABS - R$ {0}", FreioAbs);
+            } 
+        }
+
+        public string TextoArCondicionado {
+            get {
+                return string.Format("Ar Condicionado - R$ {0}", ArCondicionado);
+            }
+        }
+
+        public string TextoMp3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", Mp3Player);
+            }
+        }
+
+        bool temFreioAbs;
+        public bool TemFreioAbs {
+            get {
+                return temFreioAbs;
+            }
+            set
+            {
+                temFreioAbs = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temArCondicionado;
+        public bool TemArCondicionado
+        {
+            get
+            {
+                return temArCondicionado;
+            }
+            set
+            {
+                temArCondicionado = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        bool temMp3Player;
+        public bool TemMp3Player
+        {
+            get
+            {
+                return temMp3Player;
+            }
+            set
+            {
+                temMp3Player = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+            }
+        }
+
+        public string ValorTotal {
+            get {
+                return string.Format("Valor Total: R$ {0}", 
+                    Veiculo.Preco
+                    + (TemFreioAbs?FreioAbs:0)
+                    + (TemArCondicionado?ArCondicionado:0)
+                    + (TemMp3Player? Mp3Player:0));
+            }
+        }
+
+        public DetalhesView(Veiculo veiculo)
+        {
+            InitializeComponent();
+
+            this.Veiculo = veiculo;
+            this.BindingContext = this;
+        }
+
+        private async void buttonProximo_Clicked(object sender, EventArgs e)
+        {
+
+
+            await Navigation.PushAsync(new AgendamentoView(this.Veiculo));
+        }
+    }
+}
---
+++ TestDrive/TestDrive/Views/ListagemView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+    public class Veiculo
+    {
+        public string Modelo { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado { get { return string.Format("R$ {0}", Preco); } }
+    }
+
+    public partial class ListagemView : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+
+        public ListagemView()
+        {
+            InitializeComponent();
+
+            this.Veiculos = new List<Veiculo>()
+            {
+                new Veiculo { Modelo = "Azera V6", Preco = 60000 },
+                new Veiculo { Modelo = "Fiesta 2.0", Preco = 50000},
+                new Veiculo { Modelo = "HB20 S", Preco = 40000}
+            };
+
+            this.BindingContext = this;
+        }
+
+        private async void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo) e.Item;
+
+            await Navigation.PushAsync(new DetalhesView(veiculo));
+        }
+    }
+}
